15a1365d473918f1f97f63259e18575061ea5b10
castielhzh <328431230@qq.com>
2018-01-27 09:16:09 +0800

    add a doctest file

---
+++ doctest.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 04 21:27:13 2017
+
+@author: castiel
+"""
+
+def fact(n):
+    '''
+    >>> fact(1)
+    1
+    >>> fact(3)
+    6
+    >>> fact(-1)
+    Traceback(most recent call last):
+        ...
+    ValueError
+    '''
+    if n<1 :
+        raise ValueError()
+    if n==1:
+        return 1
+    return n*fact(n-1)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
